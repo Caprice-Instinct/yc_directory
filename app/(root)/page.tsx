03e62ec9fbe332925123dcd20a155ce9f39fc82a
Caprice-Instinct <linetwangui04@gmail.com>
2025-03-01 05:33:18 +0300

    wip: fetch startup data from sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -7,41 +9,8 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name: "John Doe" },
-      _id: 1,
-      description: "This is the description",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGAEsWIViVxbfUAoET5lGjTmHzJsQ7-ovwYw&s",
-      category: "This is the category",
-      title: "This is the title",
-    },
-    {
-      _createdAt: new Date(),
-      views: 120,
-      author: { _id: 2, name: "Alice Smith" },
-      _id: 2,
-      description: "Exploring the beauty of nature through photography.",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGAEsWIViVxbfUAoET5lGjTmHzJsQ7-ovwYw&s",
-      category: "Photography",
-      title: "Capturing Nature's Wonders",
-    },
-    {
-      _createdAt: new Date(),
-      views: 85,
-      author: { _id: 3, name: "Michael Brown" },
-      _id: 3,
-      description: "A deep dive into the latest advancements in AI.",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGAEsWIViVxbfUAoET5lGjTmHzJsQ7-ovwYw&s",
-      category: "Technology",
-      title: "The Future of Artificial Intelligence",
-    },
-  ];
+
+  const posts = await client.fetch(STARTUPS_QUERY);
 
   return (
     <>
@@ -62,7 +31,7 @@ export default async function Home({
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupTypeCard, index: number) => (
+            posts.map((post: StartupTypeCard) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
